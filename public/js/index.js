77793c9e77485200dbe7f62dc24e398e53c46d4c
parth <parthgoel007@gmail.com>
2019-07-16 21:07:53 +0530

    add css for chat app

--- public/js/index.js
+++ public/js/index.js
@@ -36,11 +36,13 @@ socket.on('newLocationMessage',function(message){
 jQuery('#message-form').on('submit',function(e){
     e.preventDefault();
 
+    var messageTextbox=jQuery('[name=message]');
+
     socket.emit('createMessage',{
         from:'User',
-        text:jQuery('[name=message]').val()
+        text:messageTextbox.val()
     },function(){
-
+        messageTextbox.val('');
     });
 });
 
@@ -51,14 +53,19 @@ locationButton.on('click',function(){
         return alert('Geolocation not supported by your browser');
     }
 
+    locationButton.attr('disabled','disabled').text('Sending Location...');
+
     navigator.geolocation.getCurrentPosition(function(position){
    //      console.log(position);
+   locationButton.removeAttr('disabled').text('Send Location');
+
    socket.emit('createLocationMessage',{
       latitude: position.coords.latitude,
       longitude: position.coords.longitude
    });
 
     },function(){
+        locationButton.removeAttr('disabled').text('Send Location');
         alert('Unable to fetch location');
     });
 });
